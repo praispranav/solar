971dcec00913037fa5b37bd656c2116c368bbe55
praispranav <praispranav@gmail.com>
2022-12-23 23:43:51 +0530

    Added Routes to Servce

--- server.js
+++ server.js
@@ -16,31 +16,31 @@ const generateRoutes = (routes) => {
 };
 
 const ROUTES = {
+  form: "/form",
+
+  seeIfUQualify: "/form/see-if-qualify",
+  monthlyElectricBill: "/form/monthly-electric-bill",
+  homeShades: "/form/home-shades",
+  homeOwner: "/form/home-owner",
+  nameEmail: "/form/name-email",
+  congrats: "/form/congrats",
+
+  callland: "/callland",
+  topSolarCompanies: "/top-solar-companies",
+  californiaPrivacyNotice: "/california-privacy-notice",
+  privacyPolicy: "/privacy-policy",
+  termsAndCondition: "/terms-and-condition",
 };
 
 app.use(express.static(path.join(__dirname, "/build")));
 
-// const fullModuleChild = Object.values(ROUTES.full.children);
-// const shortModuleChild = Object.values(ROUTES.short.children);
-// const callModuleChild = Object.values(ROUTES.call.children);
+const pages = Object.values(ROUTES);
 
-// fullModuleChild.forEach((i) => {
-//   app.get(i, (req, res) => {
-//     res.sendFile(path.join(__dirname + "/build/index.html"));
-//   });
-// });
-
-// shortModuleChild.forEach((i) => {
-//   app.get(i, (req, res) => {
-//     res.sendFile(path.join(__dirname + "/build/index.html"));
-//   });
-// });
-
-// callModuleChild.forEach((i) => {
-//   app.get(i, (req, res) => {
-//     res.sendFile(path.join(__dirname + "/build/index.html"));
-//   });
-// });
+pages.forEach((i) => {
+  app.get(i, (req, res) => {
+    res.sendFile(path.join(__dirname + "/build/index.html"));
+  });
+});
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "/build/index.html"));
